616e4676421b266bdc18e68700904d90896a2901
Renan <112997019+Renan-Rezende@users.noreply.github.com>
2023-06-14 16:08:16 -0300

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -2,18 +2,40 @@
 
 function inverter(palavra) {
 
-  var palavraInvertida = ""
+   var palavraInvertida = ""
 
-  for (var i = palavra.length - 1; i >= 0; i--) {
+   for (var i = palavra.length - 1; i >= 0; i--) {
+     palavraInvertida += palavra[i]
+  }
 
-    palavraInvertida += palavra[i]
+  console.log(palavraInvertida)
+}
+
+Iverter("Otario")
+
+
+
+// FUNÇÃO  Frequencia de valores 
+
+function frequencia(matriz) {
+
+  var objeto = {}
+
+  for (let i = 0; i < matriz.length; i++) {
+
+    if (matriz[i] in objeto) {
+
+      objeto[matriz[i]] += 1
+    } else {
+
+      objeto[matriz[i]] = 1
+    }
 
   }
 
-
+  console.log(objeto)
 
 }
 
-
-// FUNÇÃO  Inverter String
+frequencia([1, 2, 3, 1, 1, 3, 3, 2, 2, 5, 4, 5, 4])
 
