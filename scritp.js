fa24d869dd799094265148725ee92639f2451c09
Renan <112997019+Renan-Rezende@users.noreply.github.com>
2023-06-14 15:25:47 -0300

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,19 @@
+// FUNÇÃO  Inverter String
+
+function inverter(palavra) {
+
+  var palavraInvertida = ""
+
+  for (var i = palavra.length - 1; i >= 0; i--) {
+
+    palavraInvertida += palavra[i]
+
+  }
+
+
+
+}
+
+
+// FUNÇÃO  Inverter String
+
